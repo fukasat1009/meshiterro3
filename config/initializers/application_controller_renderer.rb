260d8c907c2b1c1ee9ddea0a0ddc41e5b35425e7
津田恭平 <tsudakyouhei@kumadon.local>
2020-05-02 18:17:02 +0900

    update

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f8050f9d6ef75ac2fa78bd4149cfa40228e7f4107416664f5b27ede11272e7f62e515e58124ca3c58952bd4ec4f3f3626c88e9672a5c81410137d590cf6a0575'
