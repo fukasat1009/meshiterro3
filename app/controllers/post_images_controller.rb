a92e3a6c829e2ae516ec3c01e5c7708e9a3d292d
津田恭平 <tsudakyouhei@kumadon.local>
2020-05-02 23:38:45 +0900

    update

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -15,8 +15,9 @@ class PostImagesController < ApplicationController
     end
     def show
         @post_image = PostImage.find(params[:id])
-      
+        @post_comment = PostComment.new
     end
+
     private
     def post_image_params
         params.require(:post_image).permit(:shop_name, :image, :caption)
